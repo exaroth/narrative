e1176b18e982689dee4f6fc58cf0c57e34330942
Konrad Wasowicz <exaroth@gmail.com>
2026-03-30 19:35:23 +0200

    Fix printouts in check dict script

--- check-dict.go
+++ check-dict.go
@@ -24,12 +24,16 @@ func main() {
 	}
 
 	repo_result := repo.LookupWords(word)
+
 	var phonemes map[string]uint32
 	if len(repo_result) > 0 {
 		if len(repo_result) > 1 {
 			fmt.Printf("Repository returned more that one result for word %s: %v", word, repo_result)
 		}
 		phonemes = repo_result[0]
+	} else {
+		fmt.Printf("Word %s not found\n", word)
+		return
 	}
 	fmt.Println("Results for word ", word)
 	for phoneme, k := range phonemes {
@@ -40,6 +44,4 @@ func main() {
 		var tags = repo.LookupTags(word, phoneme)
 		fmt.Printf("   Tags:  %s\n", strings.Join(tags, ", "))
 	}
-	fmt.Println(phonemes)
-
 }
