1ac5297e34dcf6a4937264087602a9c1375f8e77
Konrad Wasowicz <exaroth@gmail.com>
2026-04-01 18:31:40 +0200

    Add tests

---
+++ pkg/preprocessor/speech_test.go
@@ -0,0 +1,40 @@
+package preprocessor
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExpandingContractions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "He'll be, I haven't any idea",
+			expected: "He will be, I have not any idea",
+		},
+		{
+			input:    "I could've done more but i won't",
+			expected: "I could have done more but i won't",
+		},
+		{
+			input:    "I can't",
+			expected: "I can't",
+		},
+		{
+			input:    "I'd like some",
+			expected: "I would like some",
+		},
+	}
+	for idx, test := range tests {
+		testname := fmt.Sprintf("expand contractions: %d", idx)
+		t.Run(testname, func(t *testing.T) {
+
+			result, _ := expandContractions(test.input)
+			if result != test.expected {
+				t.Errorf("got %s, want %s", result, test.expected)
+			}
+		})
+	}
+}
