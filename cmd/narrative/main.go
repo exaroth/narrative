6072548248b4c9f39a90cccf0c22ac05f059c2be
Konrad Wasowicz <exaroth@gmail.com>
2026-04-29 21:41:34 +0200

    Update log file perms

--- cmd/narrative/main.go
+++ cmd/narrative/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/exaroth/narrative/internal/narrative"
@@ -10,7 +11,8 @@ import (
 func init() {
 	debug := os.Getenv("DEBUG")
 	if len(debug) > 0 {
-		f, err := os.OpenFile("./narrative.log", os.O_WRONLY|os.O_CREATE, 0655)
+		f, err := os.OpenFile("./narrative.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0655)
+		fmt.Println(f)
 		if err != nil {
 			panic(err)
 		}
