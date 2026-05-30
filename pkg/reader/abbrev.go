6a77f7acacca1bb1f8e48062c4a652558a85bd5e
Konrad Wasowicz <exaroth@gmail.com>
2026-05-30 14:24:28 +0200

    Update abbreviations

--- pkg/reader/abbrev.go
+++ pkg/reader/abbrev.go
@@ -9,37 +9,37 @@ import (
 
 var (
 	ABBREV_CASE_I = `\b(adj|asst|ave|bld|bldg|blvd|bros|btw|capt|cmdr|co|col|conn|corp|cpl|` +
-		`dec|dept|dr|drs|e[.]g|eg|feb|ft|esq|gov|hon|hosp|hr|hrs|hway|` +
-		`gen|hwy|i[.]e|ie|inc|insp|jan|jr|jul|lt|ltd|maj|mar|mass|may|md|` +
-		`max|min|mr|mrs|ms|msgr|messrs|mmes|mses|miss|nebr|nev|no|nos|` +
+		`dec|dept|dr|drs|e[.]g|eg|feb|ft|esq|gov|hon|hosp|hr|hrs|hway|jun|` +
+		`gen|hwy|i[.]e|ie|inc|insp|jan|jr|jul|lt|ltd|maj|mar|mass|may|md|nov|` +
+		`max|min|mr|mrs|ms|msgr|messrs|mmes|mses|miss|nebr|nev|nos|apr|` +
 		`nr|oct|ok|ph[.]d|phd|ny|penn|pls|prof|n[.]y|pvt|ref|rev|rep|sec|sep|` +
-		`sept|sgt|sr|st|tenn|tex|univ|us|u[.]s|ver|vs|fig|brig|att|sen|adm` +
+		`sept|sgt|sr|st|tenn|tex|univ|us|u[.]s|ver|vs|fig|brig|att|sen|adm|aug` +
 		`)\b([.]?)`
 	ABBREV_CASE_I_RE = regexp.MustCompile(`(?i)` + ABBREV_CASE_I)
 
-	ABBREV_CASE_S_RE = regexp.MustCompile(`\b(c[.]|s[.]|p[.])(\s|$)`)
+	ABBREV_CASE_S_RE = regexp.MustCompile(`\b(c[.]|s[.]|p[.]|v[.]|no[.])(\s|$)`)
 )
 
 var ABBREV_MAP = map[string]string{
 	"adj": "adjective", "asst": "assistant", "ave": "avenue", "bld": "building", "bldg": "building",
 	"blvd": "boulevard", "bros": "brothers", "btw": "by the way", "capt": "captain", "cmdr": "commander",
 	"co": "company", "col": "colonel", "conn": "connection", "corp": "corporation", "cpl": "corporal",
-	"dec": "december", "dept": "department", "dr": "doctor", "drs": "doctor's", "e.g": "for example",
-	"eg": "for example", "feb": "february", "ft": "featuring", "esq": "esquire", "gov": "government",
+	"dec": "December", "dept": "department", "dr": "doctor", "drs": "doctor's", "e.g": "for example",
+	"eg": "for example", "feb": "February", "ft": "featuring", "esq": "esquire", "gov": "government",
 	"hon": "honorable", "hosp": "hospital", "hr": "hour", "hrs": "hours", "hway": "highway",
 	"gen": "general", "hwy": "highway", "i.e": "that is", "ie": "that is", "inc": "incorporated",
-	"insp": "inspector", "jan": "january", "jr": "junior", "jul": "july", "lt": "lieutenant",
-	"ltd": "limited", "maj": "major", "mar": "march", "mass": "massachusetts", "may": "may",
+	"insp": "inspector", "jan": "January", "jr": "junior", "Jul": "July", "lt": "lieutenant",
+	"ltd": "limited", "maj": "major", "mar": "March", "mass": "massachusetts", "may": "may",
 	"md": "mediacal doctor", "max": "maximum", "min": "minimum", "mr": "mister", "mrs": "missus",
 	"ms": "miss", "msgr": "monsignor", "messrs": "misters", "mmes": "mesdames", "mses": "misses",
-	"miss": "miss", "nebr": "nebraska", "nev": "nevada", "no": "number", "nos": "numbers",
-	"nr": "number", "oct": "october", "ok": "ok", "ph.d": "doctor of physics",
+	"miss": "miss", "nebr": "nebraska", "nev": "nevada", "no.": "number", "nos": "numbers",
+	"nr": "number", "oct": "October", "ok": "ok", "ph.d": "doctor of physics", "aug": "August",
 	"phd": "doctor of physics", "ny": "new york", "penn": "pennsylvania", "pls": "please",
-	"prof": "professor", "n.y": "new york", "pvt": "private", "ref": "reference",
-	"rev": "reverend", "rep": "representative", "sec": "second", "sep": "separate", "sept": "september",
+	"prof": "professor", "n.y": "new york", "pvt": "private", "ref": "reference", "nov": "November",
+	"rev": "reverend", "rep": "representative", "sec": "second", "sep": "September", "sept": "September",
 	"sgt": "sergeant", "sr": "senior", "st": "street", "tenn": "tennessee", "tex": "texas", "univ": "university",
 	"us": "united states", "u.s": "united states", "ver": "version", "vs": "versus", "fig": "figure",
-	"brig": "brigadier", "att": "attorney", "sen": "senator", "adm": "admiral",
+	"brig": "brigadier", "att": "attorney", "sen": "senator", "adm": "admiral", "apr": "April", "jun": "June",
 	"c.": "cent", "s.": "shilling", "p.": "pence", "v.": "version",
 }
 
---
+++ pkg/reader/abbrev_test.go
@@ -0,0 +1,39 @@
+package reader
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExpandingAbbreviations(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "asst. to dr. Kleimer was Donald jr.",
+			expected: "assistant to doctor Kleimer was Donald junior",
+		},
+		{
+			input:    "50 c. is not 50 C.",
+			expected: "50 cent is not 50 C.",
+		},
+		{
+			input:    "rev Johnson was born on first of jan.",
+			expected: "reverend Johnson was born on first of January",
+		},
+		{
+			input:    "No, v. 1.2.3 was the last no. that mattered",
+			expected: "No, version 1.2.3 was the last number that mattered",
+		},
+	}
+	for idx, test := range tests {
+		testname := fmt.Sprintf("expanding abbrev: %d", idx)
+		t.Run(testname, func(t *testing.T) {
+			result := ExpandAbbreviations(test.input)
+			if result != test.expected {
+				t.Errorf("got %s, want %s", result, test.expected)
+			}
+		})
+	}
+}
